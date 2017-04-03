5b514f7b219bd6b3e1bf250a79f70b3952a37665
Tomasz Buczek <szalejacykwadrat@gmail.com>
2017-04-03 19:51:57 +0200

    EatDaPooPooGame

--- Engine/Game.h
+++ Engine/Game.h
@@ -34,17 +34,39 @@ public:
 private:
 	void ComposeFrame();
 	void UpdateModel();
+
 	/********************************/
 	/*  User Functions              */
+	void DrawFace(int x, int y);
+	void DrawPoo(int x, int y);
+	void DrawGameOver(int x, int y);
+	void DrawTitleScreen(int x, int y);
+	int ClampScreenX(int x, int width);
+	int ClampScreenY(int y, int height);
+	bool IsColliding(int x0, int y0, int width0, int height0,
+		int x1, int y1, int width1, int height1);
 	/********************************/
 private:
 	MainWindow& wnd;
 	Graphics gfx;
 	/********************************/
 	/*  User Variables              */
-	double x = 400;
-	double y = 300;
-	int dx = 0;
-	int dy = 0;
+	int dudeX = 400;
+	int dudeY = 300;
+	int dudeWidth = 20;
+	int dudeHeight = 20;
+	int poo0X = 300;
+	int poo0Y = 300;
+	bool poo0IsEaten = false;
+	int poo1X = 300;
+	int poo1Y = 100;
+	bool poo1IsEaten = false;
+	int poo2X = 100;
+	int poo2Y = 200;
+	bool poo2IsEaten = false;
+	int pooWidth = 24;
+	int pooHeight = 24;
+	bool isStarted = false;
+
 	/********************************/
 };
